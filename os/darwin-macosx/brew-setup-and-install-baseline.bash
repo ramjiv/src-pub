5613750accf18df14a5b0fbb22f67b7b795948ed
Ramji V <ramji@abetterconsultancy.co>
2020-09-11 17:51:43 +0800

    brew-setup-and-install-baseline.bash

--- os/darwin-macosx/brew-setup-and-install-baseline.bash
+++ os/darwin-macosx/brew-setup-and-install-baseline.bash
@@ -7,7 +7,12 @@ PATH=/bin:/usr/bin:/usr/local/bin:/usr/sbin:/sbin:$HOME/bin:$HOME/.local/bin
 export PATH
 
 ## homebrew
-brew install golang wget awscli firebase-cli git jq mutt neovim openjdk powerline-go python starship svn vim jsonlint node npm exa fzf tmux youtube-dl direnv jump autoenv
+brew install golang wget awscli firebase-cli git jq mutt neovim openjdk powerline-go python starship \ 
+ svn vim jsonlint node npm exa fzf tmux youtube-dl direnv jump autoenv bat
+
+# OMFG - bat is lifechanging
+# https://github.com/sharkdp/bat
+
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font font-fira-code-nerd-font font-fira-mono-nerd-font font-jetbrains-mono-nerd-font 
 brew cask install font-inconsolata-go-nerd-font font-fira-sans-extra-condensed font-code-new-roman-nerd-font
